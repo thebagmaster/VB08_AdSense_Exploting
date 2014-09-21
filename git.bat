5f7047e0b8223f8d7684a1178ac10278f565dd06
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 23:17:05 -0500

    Added fake website used.

--- git.bat
+++ git.bat
@@ -1,6 +1,5 @@
-git.exe init
 git.exe add .
-git.exe commit -m "First Commit"
+git.exe commit -m "Added fake website used."
 git.exe remote add origin https://github.com/thebagmaster/VB08_AdSense_Exploting.git
 git.exe pull https://github.com/thebagmaster/VB08_AdSense_Exploting.git
 git.exe push origin master
